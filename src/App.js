9029ff5d489e6fd92454b1f268805e6bc437730f
shruti garg <hellocoders8213@gmail.com>
2024-02-07 13:10:43 +0530

    configured features to build

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App text-green-800 text-2xl">
+    <div className="App text-blue-800 text-2xl">
      Hello world
     </div>
   );
