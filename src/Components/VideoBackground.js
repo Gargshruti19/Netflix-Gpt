0337cf7b452a4f5c28b93d84af7504abebdb12fc
shruti garg <hellocoders8213@gmail.com>
2024-02-09 11:18:26 +0530

    set video background on browse page

--- src/Components/VideoBackground.js
+++ src/Components/VideoBackground.js
@@ -7,13 +7,13 @@ const VideoBackground = ({ movieId }) => {
 	useMovieTrailer(movieId);
 
 	return (
-		<div>
+		<div className="w-screen">
 			<iframe
-				width="560"
-				height="315"
-				src={VIDEO_URL + trailer?.key}
+				className="w-screen aspect-video"
+				src={VIDEO_URL + trailer?.key + "?&autoplay=1&mute=1&loop=1"}
 				title="YouTube video player"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				allowFullScreen
 			></iframe>
 		</div>
 	);
--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -1,14 +1,17 @@
+import { FaPlay } from "react-icons/fa6";
+import { FiInfo } from "react-icons/fi";
+
 const VideoTitle = ({ title, overview }) => {
 	return (
-		<div className="pt-36 px-16">
-			<h1 className="text-6xl font-bold">{title}</h1>
-			<p className="py-6 text-lg w-1/3">{overview}</p>
+		<div className="w-screen aspect-video pt-[20%] px-24 absolute bg-gradient-to-r from-black ">
+			<h1 className="text-6xl font-bold text-white">{title}</h1>
+			<p className="py-6 text-lg w-1/3 text-white">{overview}</p>
 			<div className="flex space-x-2">
-				<button className="bg-white text-black px-12 py-3 border-2 border-black rounded-md text-2xl">
-					▶️ Play
+				<button className="bg-white text-black px-10 py-3 rounded-md text-xl font-sans flex items-center justify-center gap-2 font-semibold hover:bg-opacity-80">
+					<FaPlay /> Play
 				</button>
-				<button className="bg-gray-700 text-white px-12 py-3  rounded-md text-2xl opacity-60 font-bold">
-					More Info
+				<button className=" bg-gray-700 text-white px-10 py-3  rounded-md text-xl bg-opacity-50 font-sans flex items-center justify-center gap-2 hover:bg-opacity-70">
+					<FiInfo /> More Info
 				</button>
 			</div>
 		</div>
