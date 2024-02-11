0b01b69fa69defae7706b2303c1d9fe59111c8a5
shruti garg <hellocoders8213@gmail.com>
2024-02-11 18:38:47 +0530

    small change

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -3,7 +3,6 @@ import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import usePopularMovies from "../hooks/usePopularMovies";
 import useTopRatedMovies from "../hooks/useTopRatedMovies";
 import useUpcomingMovies from "../hooks/useUpcomingMovies";
-import useShowMovieInfo from "../hooks/useShowMovieInfo";
 import GptSearch from "./GptSearch";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
@@ -16,7 +15,6 @@ const Browse = () => {
 	usePopularMovies();
 	useTopRatedMovies();
 	useUpcomingMovies();
-	useShowMovieInfo();
 	return (
 		<div>
 			<Header />
